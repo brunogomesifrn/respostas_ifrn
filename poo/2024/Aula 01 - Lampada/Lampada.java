a57367a75e99521868bb4afa330be24ddd02aecd
Bruno Gomes de Araujo <2729795@ifrn.local>
2024-07-04 17:38:53 -0300

    poo

---
+++ poo/2024/Aula 01 - Lampada/Lampada.java
@@ -0,0 +1,62 @@
+
+public class Lampada {
+    private int identificador;
+    private String tipo;
+    private String estado;
+    
+    // Construtores
+    
+    // Construtor Vazio
+    public Lampada(){
+        this.estado = "Apagada";
+    }
+    
+    // Construtor Parametrizado
+    public Lampada(int identificador, String tipo){
+        this.identificador = identificador;
+        this.tipo = tipo;
+        this.estado = "Apagada";
+    }
+    
+    
+    
+    public void acender(){
+        this.estado = "Acender";
+    }
+    
+    public void apagar(){
+        this.estado = "Apagar";
+    }
+    
+  
+    // Encapsulamento - GET e SET
+    
+    public int getIdentificador(){
+        return this.identificador;
+    }
+    
+    public void setIdentificador(int identificador){
+        this.identificador = identificador;
+    }
+    
+    public String getTipo(){
+        return this.tipo;
+    }
+    
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+    
+    public String getEstado(){
+        return this.estado;
+    }
+    
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+    
+    public String toString(){
+        return "ID: "+this.identificador+" Tipo: "+this.tipo;
+    }
+    
+}
---
+++ poo/2024/Aula 01 - Lampada/TesteLampada.java
@@ -0,0 +1,46 @@
+
+public class TesteLampada {
+    public static void main(String args[]){
+        Lampada lampada1 = new Lampada();
+        
+        lampada1.setIdentificador(2);
+        lampada1.setTipo("Fluorescente");
+        
+        System.out.println(lampada1.getIdentificador());
+        System.out.println(lampada1.getTipo());
+        System.out.println(lampada1.getEstado());
+        
+        
+        System.out.println(lampada1.getEstado());
+        lampada1.acender();
+        System.out.println(lampada1.getEstado());
+        lampada1.apagar();
+        System.out.println(lampada1.getEstado());
+        
+        Lampada lampada2 = new Lampada(1, "Led");
+        
+        Lampada lampada3 = new Lampada(3, "Led");
+        lampada3.acender();
+        System.out.println(lampada3.getIdentificador());
+        System.out.println(lampada3.getTipo());
+        System.out.println(lampada3.getEstado());
+        
+        Lampada lampada4 = new Lampada();
+        lampada4.setIdentificador(4);
+        lampada4.setTipo("Fluorescente");
+        System.out.println(lampada4.getIdentificador());
+        System.out.println(lampada4.getTipo());
+        System.out.println(lampada4.getEstado());
+        
+        System.out.println(lampada4);
+        
+        
+        /*
+        
+        
+        */
+        
+        
+        
+    }
+}
