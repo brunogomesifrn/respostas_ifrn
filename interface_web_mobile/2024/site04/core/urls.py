7746b11276477c840a30fa97ae6359c7a8e9a025
Bruno Gomes de Araujo <2729795@ifrn.local>
2024-12-13 14:38:18 -0300

    CRUD Interface

--- interface_web_mobile/2024/site04/core/urls.py
+++ interface_web_mobile/2024/site04/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import areas, area_cadastro
 
 urlpatterns = [
-    
+    path('areas/', areas, name='areas'),
+    path('area_cadastro/', area_cadastro, name='area_cadastro'),
 ]
--- interface_web_mobile/2024/site04/core/views.py
+++ interface_web_mobile/2024/site04/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Area
 
-# Create your views here.
+def areas(request):
+    areas = Area.objects.all()
+    context = {
+        'areas': areas
+    }
+
+    return render(request, 'areas.html', context)
+
+def area_cadastro(request):
+    return render(request, 'area_cadastro.html')
