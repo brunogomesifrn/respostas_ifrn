c38ac4cd886e60b8631fe9203f1c7d6d2795bc78
Bruno Gomes <bruno.gomes@ifrn.edu.br>
2025-02-25 16:48:45 -0300

    Atualizacao do Projeto

--- django/aulas/2024_Sistemas_Corporativos/autenticacao/core/views.py
+++ django/aulas/2024_Sistemas_Corporativos/autenticacao/core/views.py
@@ -13,45 +13,58 @@ def index(request):
     return render(request, 'index.html')
 
 
-def perfil(request):
-    
-context = {
-
-'nome': request.SESSION['nome']
-'email': request.SESSION['email']
-}
-    return render(request, 'perfil.html')
 
 def registro(request):
+    '''
     form = UsuarioCriarForm(request.POST or None)
-
     if form.is_valid():
         form.save()
-        return redirect('index')
-    
+        return redirect('login')
     context = {
         'form': form
     }
-    return redirect('login')
-    return render(request, 'registro.html', context)
+    '''
+    if request.POST:
+
+        usuario = {
+        'nome': request.POST['nome'],
+        'cpf': request.POST['cpf'],
+        'telefone': request.POST['telefone'],
+        'datanascimento': request.POST['datanascimento'],
+        'email': request.POST['email'],
+        'password': request.POST['password']
+        }
+        response = requests.post("http://localhost:3000/signup", data = json.dumps(usuario), headers={"Content-Type": "application/json"})
+        if response.json()['msg'] == 'Usuário cadastrado com sucesso':
+            return redirect('login')        
+    
+    return render(request, 'registro.html')
+
+
+
 
 def autenticar(request):
     if request.POST:
         usuario = request.POST["usuario"]
         senha = request.POST["senha"]
         #user = authenticate(request, username=usuario, password=senha)
+
         autenticacao = {
             'email': usuario,
             'password': senha
         }
 
-        resposta = requests.post('http://10.41.1.179:3000/signin', 
+        resposta = requests.post('http://localhost:3000/signin', 
                 data=json.dumps(autenticacao), 
                 headers={"Content-Type": "application/json"})
         
         if('nome' in resposta.json()):
-            request.SESSION['nome'] = resposta.json()['nome']
-            request.SESSION['email'] = resposta.json()['email']
+            '''
+            se retornou usuário, deu certo a autenticação. 
+            Então estou salvando na sessão o objeto json
+            recebido.
+            '''
+            request.session['usuario'] = resposta.json()
             
             return redirect("perfil")
         else:
@@ -59,6 +72,34 @@ def autenticar(request):
     else:
         return render(request, "login.html")
 
+
+
+
+def perfil(request):
+    '''
+    Verifica se tem a chave 'usuario' na sessão. Se tiver, 
+    é pq o usuário está autenticado e poderá abrir a página
+    de Perfil. Caso não exista, não abrirá Perfil e redirecionará
+    o usuário para login.
+    '''
+    if 'usuario' in request.session.keys():
+        context = {
+            'nome': request.session['usuario']['nome'],
+            'email': request.session['usuario']['email']
+        }   
+        return render(request, 'perfil.html', context)
+    else:
+        return redirect('login')
+
+
+
+
+
 def desconectar(request):
-    logout(request)
+    #logout(request)
+    '''
+    Excluir a chave 'usuario' da sessão,
+    desta forma o usuário estará deslogando.
+    '''
+    del request.session['usuario']
     return redirect("index")
