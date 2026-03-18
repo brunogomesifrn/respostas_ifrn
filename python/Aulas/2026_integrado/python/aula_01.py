b02366271b3b8b1eabb2203f5c13fabb57ab0399
Bruno Gomes de Araujo <2729795@ifrn.local>
2026-03-18 17:50:47 -0300

    Aula Python

--- python/Aulas/2026_integrado/python/aula_01.py
+++ python/Aulas/2026_integrado/python/aula_01.py
@@ -1,2 +1,66 @@
-print('Olá Mundo')
+print("Olá Mundo")
 
+# Comentário em 1 linha
+
+"""
+Comentário em
+várias linhas
+"""
+
+'''
+Comentário em
+várias linhas
+'''
+
+'''
+TIPOS DE DADOS
+numérico - inteiro, real
+texto - cadeia/lista de caracteres
+lógico - verdadeiro (true) ou falso (false)
+Nulo - None (objeto vazio)
+'''
+
+'''
+VARIÁVEL
+- Armazenamento temporário de dados
+- Fracamente tipado / tipagem dinâmica
+'''
+
+idade = 30
+nota = 8.5
+nome = 'BG'
+valor_1 = False
+valor_2 = True
+objeto = None
+
+print(nome)
+
+# Concatenação se usa ,
+
+print('O nome é ',nome)
+print('O nome é',nome,'e idade',idade)
+
+'''
+OPERADORES ARITMÉTICOS
++ soma
+- subtração
+* multiplicação
+/ divisão
+% resto da divião
+
+Obs.: retornam sempre valor numérico
+'''
+
+print(5+3)
+
+num_1 = 10
+print(num_1-5)
+
+num_1 = 15
+resultado = num_1*2
+print(resultado)
+
+num_1 = 20
+num_2 = 10
+resultado = num_1 % num_2
+print(resultado)
