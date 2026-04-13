967310f98999a5f93d9af27778c1ad221e8f162c
Bruno Gomes de Araujo <2729795@ifrn.local>
2026-04-13 14:32:43 -0300

    Aula Django

--- django/aulas/2026/aula_03/core/urls.py
+++ django/aulas/2026/aula_03/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import inicio, contato
+from .views import *
 
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('contato/', contato, name='contato'),
+    path('cadastro_01/', cadastro_01, name='cadastro_01'),
+    path('resposta_01/', resposta_01, name='resposta_01'),
 ]
--- django/aulas/2026/aula_03/core/views.py
+++ django/aulas/2026/aula_03/core/views.py
@@ -10,3 +10,19 @@ def inicio(request):
 
 def contato(request):
     return render(request, 'contato.html')
+
+def cadastro_01(request):
+    return render(request, 'cadastro_01.html')
+
+def resposta_01(request):
+    if request.POST:
+        nome = request.POST['nome']
+        cpf = request.POST['cpf']
+        disciplina = request.POST['disc']
+        context = {
+            'nome': nome,
+            'cpf': cpf,
+            'disciplina': disciplina
+        }
+        return render(request, 'resposta_01.html', context)
+    return render(request, 'cadastro_01.html')
