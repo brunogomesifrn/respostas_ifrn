20d98613c5143081dcf965d69733d48cc3c8556e
Bruno Gomes de Araujo <2729795@ifrn.local>
2026-04-22 11:40:24 -0300

    Aula django info4

---
+++ django/aulas/2026_info4/aula02/core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def inicial(request):
+    return render(request, 'index.html')
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def perfil(request):
+    return render(request, 'perfil.html')
