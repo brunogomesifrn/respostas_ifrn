a2a1cbf7831e56330993ed1f1c41bb13e6e8e92c
Bruno Gomes de Araujo <2729795@ifrn.local>
2026-06-01 14:39:14 -0300

    FIC Django

--- django/aulas/2026/fic/core/urls.py
+++ django/aulas/2026/fic/core/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('curso_galeria/', curso_galeria, name='curso_galeria'),
 
     path('area_listar', area_listar, name='area_listar'),
+    path('area_cadastrar', area_cadastrar, name='area_cadastrar'),
+    path('area_editar/<int:id>/', area_editar, name='area_editar'),
+    path('area_remover/<int:id>/', area_remover, name='area_remover'),
 ]
--- django/aulas/2026/fic/core/views.py
+++ django/aulas/2026/fic/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Area
 from .forms import AreaForm
 
@@ -16,4 +16,31 @@ def area_listar(request):
     context = {
         'areas': areas
     }
-    return render(request, 'areas.html', context)
+    #return render(request, 'areas.html', context)
+    return render(request, 'privado/areas.html', context)
+
+def area_cadastrar(request):
+    form = AreaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('area_listar')
+    context = {
+        'form': form
+    }
+    return render(request, 'area_cadastrar.html', context)
+
+def area_editar(request, id):
+    area = Area.objects.get(pk=id)
+    form = AreaForm(request.POST or None, instance=area)
+    if form.is_valid():
+        form.save()
+        return redirect('area_listar')
+    context = {
+        'form': form
+    }
+    return render(request, 'area_cadastrar.html', context)
+
+def area_remover(request, id):
+    area = Area.objects.get(pk=id)
+    area.delete()
+    return redirect('area_listar')
